8fb36c85c6123691ef3049e7c80308cfee12043c
Prakhar <prakhar0402@gmail.com>
2016-01-15 16:39:09 -0500

    Adding LICENSE.

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
+#    DART Lab, University at Buffalo
+#    Copyright (C) 2016  Prakhar Jaiswal <prakharj@buffalo.edu>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 # First, and before importing any Enthought packages, set the ETS_TOOLKIT
 # environment variable to qt4, to tell Traits that we will use Qt.
 import os
